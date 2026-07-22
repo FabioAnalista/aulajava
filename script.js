07a42e5905a1134409c5c6fba005ca4734372db0
Fabio <fabioaa1976@gmail.com>
2026-07-21 22:26:34 -0300

    Tabuada

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-// Pede para o usuário digitar um número
-let numeroBase = parseInt(prompt("Digite o número da tabuada que deseja calcular:"));
+script.js
+function tabuada(){
+  var num = parseInt(document.getElementById("num").value);
+  var resposta = document.getElementById('resposta');
+  var tabuada='';
 
-// Verifica se foi digitado um número válido
-if (!isNaN(numeroBase)) {
-    let resultadoFinal = "";
-
-    // Gera a tabuada até 10
-    for (let i = 1; i <= 10; i++) {
-        resultadoFinal += `${numeroBase} x ${i} = ${numeroBase * i}\n`;
-    }
-
-    // Exibe todos os resultados de uma vez
-    alert(resultadoFinal);
-} else {
-    alert("Por favor, digite um número válido!");
+  for(var count=1; count<=10 ; count++)
+   tabuada += num+" x "+count+" = "+
+               num*count+"<br />";
+  
+  resposta.innerHTML = tabuada;
 }
+console.log(tabuada);
